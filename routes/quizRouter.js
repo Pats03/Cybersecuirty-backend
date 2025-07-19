91583ab87ed9f703e45c245edff9a393f0f42265
Pats03 <ananthateja2004@gmail.com>
2025-07-19 21:57:30 +0530

    done man

--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -1,13 +1,22 @@
 const Router = require('express'); // This is already correct, but you may want to use 'express' for routing, not mongoose.
 const router = Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { getQuiz, getAllQuizzes,createQuiz,updateQuiz ,getScore,updateScore,getScoresByJobRole,deleteQuiz} = require('../controller/quizController'); // Change from ES module import to CommonJS require
+const {
+  getQuiz,
+  getAllQuizzes,
+  createQuiz,
+  updateQuiz,
+  getScore,
+  updateScore,
+  getScoresByJobRole,
+  deleteQuiz,
+} = require('../controller/quizController'); // Change from ES module import to CommonJS require
 router.get('/quiz/:role', getQuiz);
 router.get('/quiz', getAllQuizzes);
 router.post('/quiz', createQuiz);
 router.put('/quiz/:id', updateQuiz);
-router.get('/my-score', authMiddleware,getScore);
-router.post('/update-score', authMiddleware, updateScore);
-router.get('/scores-by-jobrole', authMiddleware, getScoresByJobRole);
+router.get('/my-score', getScore);
+router.post('/update-score', updateScore);
+router.get('/scores-by-jobrole', getScoresByJobRole);
 router.delete('/quiz/:id', deleteQuiz);
 module.exports = router;
