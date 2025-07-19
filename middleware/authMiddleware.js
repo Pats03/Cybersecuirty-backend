3d5e1c1b981acec3ec28d5358e2944a0c830e7f2
Pats03 <ananthateja2004@gmail.com>
2025-07-19 22:41:16 +0530

    donee

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,33 +1,33 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const {verifyJWT}= require('../utils/tokenUtils')
-const authMiddleware = async (req, res, next) => {
-  try {
-    const token =
-      req.cookies.token || req.header('Authorization')?.split(' ')[1];
-    if (!token)
-      return res
-        .status(401)
-        .json({ message: 'Unauthorized: No token provided' });
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/user');
+// const {verifyJWT}= require('../utils/tokenUtils')
+// const authMiddleware = async (req, res, next) => {
+//   try {
+//     const token =
+//       req.cookies.token || req.header('Authorization')?.split(' ')[1];
+//     if (!token)
+//       return res
+//         .status(401)
+//         .json({ message: 'Unauthorized: No token provided' });
 
-    console.log('Received Token in Middleware:', token);
+//     console.log('Received Token in Middleware:', token);
 
-    const decoded = verifyJWT(token); // Decode token
-    console.log('Decoded Token:', decoded); // Debugging
+//     const decoded = verifyJWT(token); // Decode token
+//     console.log('Decoded Token:', decoded); // Debugging
 
-    req.user = await User.findById(decoded.userId).select('-password');
+//     req.user = await User.findById(decoded.userId).select('-password');
 
-    if (!req.user) {
-      console.log('User not found in DB');
-      return res.status(401).json({ message: 'User not found' });
-    }
+//     if (!req.user) {
+//       console.log('User not found in DB');
+//       return res.status(401).json({ message: 'User not found' });
+//     }
 
-    next();
-  } catch (error) {
-    console.error('JWT Verification Error:', error);
-    res.status(401).json({ message: 'Invalid token' });
-  }
-};
+//     next();
+//   } catch (error) {
+//     console.error('JWT Verification Error:', error);
+//     res.status(401).json({ message: 'Invalid token' });
+//   }
+// };
 
 
-module.exports = authMiddleware;
+// module.exports = authMiddleware;
